f5ed4cd413493c1e000b0eb0d5680b8ac879bb0c
Iain Ballard <iain.ballard@7digital.com>
2013-02-01 14:40:06 +0000

    First blush

---
+++ src/LessStupidPath/FilePath.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace System.IO
+{
+    public class FilePath
+    {
+	    readonly List<string> _parts;
+	    readonly bool _rooted;
+
+	    /// <summary> Create a file path from a path string </summary>
+	    public FilePath(string input)
+	    {
+			_parts = new List<string>(
+				input.Split(new []{Path.AltDirectorySeparatorChar, Path.DirectorySeparatorChar, Path.PathSeparator, Path.VolumeSeparatorChar}, StringSplitOptions.RemoveEmptyEntries)
+				);
+			_rooted = Path.IsPathRooted(input);
+	    }
+
+	    public string ToPosixPath()
+	    {
+		    return (_rooted) 
+				? "/" + string.Join("/", _parts)
+				: string.Join("/", _parts);
+	    }
+
+	    public string ToWindowsPath()
+	    {
+		    return (_rooted) 
+				? string.Join("\\", WindowsDriveSpecOrFolder(), string.Join("\\", _parts.Skip(1)))
+				: string.Join("\\", _parts);
+	    }
+
+		public string ToEnvironmentalPath()
+		{
+			return PosixOS() ? ToPosixPath() : ToWindowsPath();
+		}
+		static bool PosixOS()
+		{
+			var p = (int)Environment.OSVersion.Platform;
+			return (p == 4) || (p == 6) || (p == 128);
+		}
+
+		string WindowsDriveSpecOrFolder()
+	    {
+			if (_parts.Count < 1) return "";
+
+			if (_parts[0].Length == 1) return _parts[0] + ":";
+		    return "\\" + _parts.First();
+	    }
+    }
+}
